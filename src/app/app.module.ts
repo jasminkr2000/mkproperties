c7f0f0d82b51f06997c3590c6f61776fa0cd0ce3
Jasmine Kaur <jasmin_kr2000@yahoo.com>
2020-07-28 17:09:10 +0530

    added folders and files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import{HttpClientModule} from '@angular/common/http'
+
 
 import { AppComponent } from './app.component';
+import{NavBarComponent} from './nav-bar/nav-bar.component';
+import{PropertyCardComponent} from './property/property-card/property-card.component';
+import{PropertyListComponent} from './property/property-list/property-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    PropertyCardComponent,
+    PropertyListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
